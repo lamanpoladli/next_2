9d0a61f120dd11657bffab12c9f64bc6894e0a21
lamanpoladli <lamanpoladli@gmail.com>
2023-02-28 23:44:07 +0400

    oo

---
+++ script.js
@@ -0,0 +1,13 @@
+let number=8;
+let count =0;
+for (let i=2; i<number; i++){
+    if (number%i==0){
+        count++;
+    }
+}
+if(count>0){
+    console.log("murekkebdir");
+}
+else{
+    console.log("sadedir");
+}
